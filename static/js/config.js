38ddce27925416b0d0c8bea64e48a68ab234d98d
jwhite0413 <67512469+jwhite0413@users.noreply.github.com>
2020-11-18 18:14:17 -0500

    First commit

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiamVzc3doaXRlMDQxMyIsImEiOiJja2htcGR1cXgwOGdpMnRub2R1d3NoZDF0In0.OLHV6huGFMdkDFcKHZNjog";
---
+++ static/js/logic.js
@@ -0,0 +1,7 @@
+function createMap(earthquakes) {
+    var lightmap = L.titleLayer('https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js')
+}
+
+
+<script src='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css' rel='stylesheet' />
